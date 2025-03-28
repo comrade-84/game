0f724d740e1d57fa2fda6c22ad7367d1e6709427
saliu ridwan <ridwansaliu84@gmail.com>
2025-03-28 13:10:31 +0100

    adde some js feature

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 'use strict';
-// document.querySelector('.guess') ; 
+//extracting our varibles directly from html 
 const inputValue = document.getElementById('guess');
 const checkBtn = document.getElementById('check');
 const resetBtn = document.getElementById('again');
 
+
+//creating the hidden or secrete number
 let hiddenNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
 
+//writing the functionality of the whole game
  function checker (){
+    //checking if there is a true number in the input field
     if (!inputValue.value) {
         document.querySelector('.message').textContent = `No number`;
     }
+    // a scenario in which the game is won
     else if (inputValue.value == hiddenNumber) {
             document.querySelector('.highscore').textContent = score;
         document.querySelector('.message').textContent = ` 😎😎😎correct number`;
@@ -20,18 +25,25 @@ let score = 20;
         
         
     }
+    //a scenario in which the input number is greater than the secrete number
     else if(inputValue.value > hiddenNumber){
+
+        // to cancel the game if score is less than 0
         if (score <= 0) {
            document.querySelector('.message').textContent = ` you lost the game`;
         }
        else{
          document.querySelector('.message').textContent = `⚡ Too High`;
+         // decrement the score on each failed attempt
         score--;
         document.querySelector('.score').textContent = score;
        }
         
     }
+
+    // a scenario in which the input value is less than the secrete number
     else if(inputValue.value < hiddenNumber){
+         // to cancel the game if score is less than 1
         if (score <= 0) {
             document.querySelector('.message').textContent = ` you lost the game`;
         }
@@ -44,6 +56,8 @@ let score = 20;
         
     }
 }
+
+// the reset function of the game
 function reset (){
     document.querySelector('.message').textContent = `Start guessing...` ;
     document.querySelector('body').style.backgroundColor = '#222';
@@ -54,8 +68,17 @@ function reset (){
     document.querySelector('.score').textContent = score;
     hiddenNumber = Math.trunc(Math.random() * 20 + 1);
 }
-
-
+document.addEventListener('keydown', function(r){
+    if(r.key === "Enter"){
+        if(inputValue.value){
+            checker();
+            inputValue.value = "";
+            
+        }
+    }
+    
+    
+});
 checkBtn.addEventListener('click',
     function(){
        checker() ;
@@ -67,4 +90,4 @@ resetBtn.addEventListener("click",
     function(){
         reset();
     }
-)
+);
